9eb73b4f85d314fcc38f387e812b2abd0245756a
Sai Charan Tej <38549543+saicharantej99@users.noreply.github.com>
2019-05-26 20:01:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -156,7 +156,7 @@ function createDownloadLink(blob) {
 		  };
 		  var fd=new FormData();
 		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","upload.ipynb",true);
+		  xhr.open("POST","upload",true);
 		  xhr.send(fd);
 	})
 	li.appendChild(document.createTextNode (" "))//add a space in between
